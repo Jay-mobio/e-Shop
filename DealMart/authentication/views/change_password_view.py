f95bab1670bb0d9b0157affd2c2ff8eff98e9782
Jay Kapadiya <jayk.mobio@gmail.com>
2022-08-04 10:20:09 +0530

    removed unnecessary things

--- DealMart/authentication/views/change_password_view.py
+++ DealMart/authentication/views/change_password_view.py
@@ -1,10 +1,9 @@
-import django
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import render, redirect
 from django.views import View
-from django.contrib.auth import login
+
 
 
 class ChangePasswordView(View):
