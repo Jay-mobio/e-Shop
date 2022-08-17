0027f61060ef10915df124fc2acadc1032ebf0af
Jay Kapadiya <jayk.mobio@gmail.com>
2022-08-17 11:32:25 +0530

    Made relations between product, inevntory and category models

--- DealMart/products/admin.py
+++ DealMart/products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from products.models import Products,Category,Inventory
 # Register your models here.
+admin.site.register(Products)
+admin.site.register(Category)
+admin.site.register(Inventory)
--- DealMart/products/models.py
+++ DealMart/products/models.py
@@ -1,25 +1,38 @@
+import re
 from django.db import models
+from user_module.models import User
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    date_created = models.DateTimeField(auto_now_add = True, null = True)
+    date_at = models.DateTimeField(auto_now_add = True, null = True)
+    updated_at = models.DateTimeField(auto_now_add = True, null = True)
+    created_by = models.ForeignKey (User,on_delete=models.CASCADE, related_name="created_category")
+    updated_by = models.ForeignKey (User,on_delete=models.CASCADE, related_name="updated_category")
     
     def __str__(self):
         return self.name
 
 class Products(models.Model):
     name = models.CharField(max_length=255)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
-    price = models.IntegerField(max_length=18)
-    image = models.ImageField(null=True, blank=True)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    price = models.IntegerField(blank=True, null=True)
+    image = models.ImageField(null=True, blank=True, default="deault.jpg")
     discription = models.TextField
-    date_created = models.DateTimeField(auto_now_add = True, null = True)
+    date_at = models.DateTimeField(auto_now_add = True, null = True)
+    updated_at = models.DateTimeField(auto_now_add = True, null = True)
+    created_by = models.ForeignKey (User,on_delete=models.CASCADE, related_name="created_product")
+    updated_by = models.ForeignKey (User,on_delete=models.CASCADE,related_name="updated_product")
 
     def __str__(self):
         return self.label
 
 class Inventory(models.Model):
-    product_quantity = models.IntegerField(max_length=200)
+    product_id = models.OneToOneField(Products,on_delete=models.CASCADE,primary_key=True)
+    product_quantity = models.IntegerField(blank=True, null=True)
     is_active = models.BooleanField(default=False)
+    date_at = models.DateTimeField(auto_now_add = True, null = True)
+    updated_at = models.DateTimeField(auto_now_add = True, null = True)
+    created_by = models.ForeignKey (User,on_delete=models.CASCADE, related_name="created_inventory")
+    updated_by = models.ForeignKey (User,on_delete=models.CASCADE, related_name="updated_inventory")
