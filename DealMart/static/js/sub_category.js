f7423e6ff8126e19e5689bd10c27784f5ec8579d
Jay Kapadiya <jayk.mobio@gmail.com>
2022-08-25 09:41:44 +0530

    Removed comments in sub_category.js and add product template

--- DealMart/static/js/sub_category.js
+++ DealMart/static/js/sub_category.js
@@ -9,35 +9,15 @@ category.addEventListener('change', (e) => {
         .then((context) => {
             const data = context.data;
             console.log("data", data)
-            // var innerHTML = `<option value="${item.id}">${item.name}</option>`
             var select = "<select>"
             for (let i = 0; i < data.length; i++) {
                 console.log(data[i])
-                select += "<option>" + data[i].id + "</option>"
+                select += "<option>" + data[i].name + "</option>"
                 
             }
             select += "</select>"
             document.getElementById("id_sub_category").innerHTML = select
-            // data.forEach((item) => {
-            //     console.log("itme",item)
-            //     sub_category.innerHTML = `<option value="${item.id}">${item.name}</option>`
 
-            // });
 
         })
 })
-// $("#id_category").change(function () {
-
-
-//     var categoryId = $(this).val();
-//     fetch("/products/select_cat/"+categoryId+"/",{
-//         // url:
-//         // success: function (data) {
-//         //     console.log(data.subcategory)
-//         //     $("#id_sub_category").html(`
-//         //     `);
-//         // }
-//     }),
-//     .then()
-
-// });
