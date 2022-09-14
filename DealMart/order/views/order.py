2373604168a8efd74ba6ae040b7c1ccc6dd013a0
Jay Kapadiya <jayk.mobio@gmail.com>
2022-09-14 17:17:42 +0530

    Email template will be sent to the user after order

--- DealMart/order/views/order.py
+++ DealMart/order/views/order.py
@@ -1,3 +1,4 @@
+from unittest import loader
 from django.shortcuts import redirect,render
 from django.views.generic.edit import CreateView
 from django.views.generic import TemplateView
@@ -5,23 +6,29 @@ from django.core.mail import send_mail
 from django.conf import settings
 from customer.models import Cart
 from order.models import Order
+from django.template.loader import get_template,render_to_string
+from django.utils.html import strip_tags
+from django.core.mail import EmailMessage
 
 class CreateOrder(CreateView):    
     def post(self, request):
-        cart = Cart.objects.filter(created_by = request.user)
+        cart = Cart.objects.filter(created_by = request.user,is_active=True)
         order = Order.objects.create(created_by=request.user)
+        total = 0
         for i in cart:
+            total = i.product.price * i.quantity + total
             order.cart.add(i)
-
         Cart.objects.filter(created_by = request.user).update(is_active=False)
-        mess = f"Hello {request.user.first_name},\n Thank You for ordering from DealMart\n Your order id is {order.id}"
-        send_mail(
-            "Welcome to DealMart",
-            mess,
-            settings.EMAIL_HOST_USER,
-            [request.user],
-            fail_silently = False
-            ) 
+        context = {'cart':cart,'total':total}
+        message = get_template('order/order_email.html').render(context)
+        msg = EmailMessage(
+        'Order recieved',
+        message,
+        settings.EMAIL_HOST_USER,
+        [request.user],
+        )
+        msg.content_subtype ="html"
+        msg.send()
 
         return redirect("order:order_placed")
 
